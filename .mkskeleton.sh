5bff8d734dea84b958371b48b85b901d6d4ae4d7
Lilly Aizawa <83188516+jossgamerYT156@users.noreply.github.com>
2025-07-25 06:02:28 +0000

    Update .mkskeleton.sh

--- .mkskeleton.sh
+++ .mkskeleton.sh
@@ -6,5 +6,5 @@ echo "Please Wait..."
 ## Why time? so you know how long it took to create directories, duh.
 time sudo mkdir -p /nix/System/Sys && time sudo mkdir -p /nix/System/GIT && time sudo mkdir /nix/Users
 
-echo "Done Creating Main Directory Layout, Proceed Cloning Dotfiles.
-Thank you!."
+echo "Done Creating Main Directory Layout, Proceed Cloning Dotfiles to $HOME directory
+Thank you!"
